2cece47b4c0992e826b95389a0e14649b2dd5197
FathimaFaseeha <58139325+FathimaFaseeha@users.noreply.github.com>
2019-12-06 20:05:56 +0530

    Update c_report.php

--- application/controllers/c_report.php
+++ application/controllers/c_report.php
@@ -11,6 +11,8 @@ class c_report extends CI_Controller {
 
     public function index(){
 
+        $this->load->library('employee_report');
+        $this->load->model('employeeModel');
         $this->load->view('EmployeeReport/v_report', ['data' ->$data]);
         $data = $this->employeeModel->getAllData();
     }
